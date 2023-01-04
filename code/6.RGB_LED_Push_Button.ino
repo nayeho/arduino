75bc28562aa7ba03e61dea4a8581f904377ccd25
Yeho Na <83909875+nayeho@users.noreply.github.com>
2023-01-04 22:04:41 +0900

    Update 6.RGB_LED_Push_Button.ino

--- code/6.RGB_LED_Push_Button.ino
+++ code/6.RGB_LED_Push_Button.ino
@@ -1,25 +1,48 @@
+int ledRed = 13;
+int ledBlue = 12;
+int ledGreen = 11;
+int btn1 = 2;
+int btn2 = 3;
+int btn3 = 4;
+
 void setup() {
-  pinMode(13, OUTPUT);  // Red
-  pinMode(12, OUTPUT);  // Blue
-  pinMode(11, OUTPUT);  // Green
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
+  pinMode(ledRed, OUTPUT);  // Red
+  pinMode(ledBlue, OUTPUT);  // Blue
+  pinMode(ledGreen, OUTPUT);  // Green
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+  pinMode(btn3, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int buttonState1 = digitalRead(2);
-  int buttonState2 = digitalRead(3);
-  int buttonState3 = digitalRead(4);
+  int btnState1 = digitalRead(btn1);
+  int btnState2 = digitalRead(btn2);
+  int btnState3 = digitalRead(btn3);
 
   Serial.print(buttonState1);
   Serial.print(" ");
   Serial.print(buttonState2);
   Serial.print(" ");
   Serial.println(buttonState3);
+  
+  if(btnState1 == 1){
+    digitalWrite(ledRed, 1);
+  }else{
+    digitalWrite(ledRed, 0);
+  }  
+  if(btnState2 == 1){
+    digitalWrite(ledBlue, 1);
+  }else{
+    digitalWrite(ledBlue, 0);
+  }  
+  if(btnState3 == 1){
+    digitalWrite(ledGreen, 1);
+  }else{
+    digitalWrite(ledGreen, 0);
+  }
 
-  digitalWrite(13, buttonState1);
-  digitalWrite(12, buttonState2);
-  digitalWrite(11, buttonState3);
+//   digitalWrite(ledRed, buttonState1);
+//   digitalWrite(ledBlue, buttonState2);
+//   digitalWrite(ledGreen, buttonState3);
 }
